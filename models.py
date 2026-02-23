ecd4cc6212a552dc3a2f4e15988aff8913c8bd7c
anishs37 <anish.susarla@gmail.com>
2026-02-23 17:08:21 -0500

    Issue schema

--- models.py
+++ models.py
@@ -20,3 +20,14 @@ def create_project(repo_name, repo_url, language):
         "repo_url": repo_url,
         "primary_language": language
     }
+
+# --- ISSUE SCHEMA ---
+def create_issue(project_id, title, issue_url, difficulty_level):
+    return {
+        "project_id": ObjectId(project_id),
+        "title": title,
+        "issue_url": issue_url,
+        "difficulty_label": difficulty_level,
+        "is_active": True,
+        "created_at": datetime.now(datetime.timezone.utc)
+    }
