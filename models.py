fb898bcbc8a0df535ec36c9b412489481993e1c1
anishs37 <anish.susarla@gmail.com>
2026-02-23 17:03:28 -0500

    Project schema

--- models.py
+++ models.py
@@ -12,3 +12,11 @@ def create_user(username, email, languages, experience_level):
         "saved_issues": [],
         "created_at": datetime.now(datetime.timezone.utc)
     }
+
+# --- PROJECT SCHEMA ---
+def create_project(repo_name, repo_url, language):
+    return {
+        "repo_name": repo_name,
+        "repo_url": repo_url,
+        "primary_language": language
+    }
